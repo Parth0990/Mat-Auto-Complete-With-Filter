9b5d81c364a707e2db3f9cf88825b8509c361fef
Parth0990 <parthjoshi0990@gmail.com>
2023-06-13 10:57:37 +0530

    First Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable, map, startWith } from 'rxjs';
+
+export class filteredOption {
+  ObjId: number;
+  ObjName: string;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  myControl = new FormControl('');
+  filteredOption: Observable<filteredOption[]>;
+  OptionObj = [
+    {
+      ObjId: 1,
+      ObjName: 'test'
+    },
+    {
+      ObjId: 2,
+      ObjName: 'Demo'
+    }
+  ]
+ constructor(){}
+  ngOnInit(){
+    this.filteredOption = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this.filter(value || '')),
+    );
+  }
+
+  filter(val): filteredOption[]{
+    const filterVal = val.toLowerCase();
+    return this.OptionObj.filter((item) => item["ObjName"].toLowerCase().includes(filterVal));
+  }
+
+  displayfn(subject: filteredOption){
+    return subject && subject.ObjName ? subject.ObjName : undefined;
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatAutocompleteModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
